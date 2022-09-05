9ecbcd9a1e26c0bf0c1a9534fdcf53da7f0e4d7b
Christine Destacamento <jddestacamento@iskolarngbayan.pup.edu.ph>
2022-09-05 20:19:26 +0800

    Fixed bug in port assignment

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import sys
 app = create_app()
 
 if __name__ == '__main__':
-	port = int(sys.argv[1]) if sys.argv[1] else 5000
+	port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
 	app.run(debug=True, port=port)
